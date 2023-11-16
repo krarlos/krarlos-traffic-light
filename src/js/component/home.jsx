6209a2c4044baea2ec6fcf2fa607bea15dc1747c
Carlos Eduardo <127683028+krarlos@users.noreply.github.com>
2023-11-16 00:24:23 +0000

    semaforo activo

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,15 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 //import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [ selectedColor, setSelectedColor] = useState ("yellow")
 	return (
 		<div className="traffic-light">
-			<div className="light red "></div>
-			<div className="light yellow "></div>
-			<div className="light green "></div>
+			<div className={"light red" + (selectedColor === "red" ? " red glow" : "" )}></div>
+			<div className={"light yellow" + (selectedColor === "yellow" ? " yellow glow" : "")}></div>
+			<div className={"light green" + (selectedColor === "green" ? "green glow" : "")}></div>
 		</div>
 	);
 };
